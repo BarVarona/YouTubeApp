a86c382801c066f55aeffd262e14e0411427e258
Bar Varona <bar13varona@gmail.com>
2022-08-20 19:14:12 +0300

    Add Menu + NavBar Components

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
+const Container = styled.div`
+  position: sticky;
+  top: 0;
+  background-color: ${({ theme }) => theme.bgLighter};
+  height: 56px;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+  padding: 0px 20px;
+`;
+
+const Search = styled.div``;
+const Input = styled.input``;
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Search>
+          <Input placeholder="Search" />
+          <SearchOutlinedIcon />
+          <Button>
+            <AccountCircleOutlinedIcon />
+            SIGN IN
+          </Button>
+        </Search>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
